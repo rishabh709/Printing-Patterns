0fb5d2044c5ffb8e20b7dc31968de8ad3545135e
rishabh709 <97673849+rishabh709@users.noreply.github.com>
2022-07-08 21:52:44 +0530

    Add files via upload

---
+++ Square_Pattern.java
@@ -0,0 +1,10 @@
+class Square_Pattern{
+	public static void main(String args[]){
+		int i,j;
+		for(i=0; i<5;i++){
+			for(j=0; j<10; j++){
+				System.out.print("#");
+			}System.out.println("");
+		}
+	}
+}
---
+++ Stairs_Pattern.java
@@ -0,0 +1,12 @@
+class Stairs_Pattern{
+	public static void main(String args[]){
+		int i,j;
+
+		for(i=1; i<11; i++){
+			for(j=0; j<i; j++){
+				System.out.print('#');
+			}System.out.println();
+		}
+
+	}
+}
